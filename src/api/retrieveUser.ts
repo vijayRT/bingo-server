b5683f157c26e1a5b17239d9546482300bab1d21
Vijay Rajasekar <vijayrajasekar1@gmail.com>
2020-10-25 02:24:12 -0400

    clean up auth

--- src/api/retrieveUser.ts
+++ src/api/retrieveUser.ts
@@ -4,28 +4,28 @@ import { Request, Response } from 'express'
 interface RetrieveUserRequestBody {
     email: string
 }
+const userDataFields = {
+    "gamesPlayed": 0,
+    "coins": 50,
+    "gameLosses": 0,
+    "gameWins": 0,
+    "avatar": "dog",
+    "coinsSpent": 0,
+    "activeTheme": "cherryblossom",
+    "ownedThemes": [] as Array<string>,
+    "ownedAvatars": [
+        "dog"
+    ]
+}
 
 export const retrieveUser = async (request: Request<any, any, RetrieveUserRequestBody, any>, response: Response) => {
-    console.log(request.body.email)
+    const {email} = request.body
     const userDocCollection = firestoreApp.collection('users')
-    const userDocQuery = userDocCollection.doc(request.body.email)
+    const userDocQuery = userDocCollection.doc(email)
     const doc = await userDocQuery.get()
     if (!doc.exists) {
-        const newUserData = {
-            "gamesPlayed": 0,
-            "coins": 50,
-            "gameLosses": 0,
-            "gameWins": 0,
-            "avatar": "dog",
-            "coinsSpent": 0,
-            "name": request.body.email.split('@')[0],
-            "activeTheme": "cherryblossom",
-            "ownedThemes": [] as Array<string>,
-            "ownedAvatars": [
-                "dog"
-            ]
-        }
-        await userDocCollection.doc(request.body.email).set(newUserData);
+        const newUserData = {...userDataFields, name: email.split("@")[0]}
+        await userDocCollection.doc(email).set(newUserData);
         response.send(newUserData)
     } else {
         response.send(doc.data())
